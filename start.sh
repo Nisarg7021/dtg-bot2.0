0bb7dfed9f901d6a37aee959ed33fdeb5e5be08a
Nisarg7021 <95787542+Nisarg7021@users.noreply.github.com>
2024-04-19 06:40:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
-echo "Cloning Koyeb branch"
-git clone -b koyeb https://github.com/Nisarg7021/DTG-linkszad-bot /DTG-linkszad-bot
-
-cd /DTG-linkszad-bot || exit
-
+if [ -z $SOURCE_CODE ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/Nisarg7021/dtg-bot2.0.git /dtg-bot2.0
+else
+  echo "Cloning Custom Repo from $SOURCE_CODE "
+  git clone $SOURCE_CODE /dtg-bot2.0
+fi
+cd /dtg-bot2.0
 pip3 install -U -r requirements.txt
-
 echo "Starting Bot...."
 python3 -m main
-
-
-
+https://github.com/
